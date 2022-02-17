a1c574df75901760dfc1cc138c0a47b37e8a3841
Furqaan Thakur <thakurfurqaan@gmail.com>
2022-02-17 13:08:16 +0530

    <a> tags replaced with <Link>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,14 @@
+import React from 'react';
+import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
+
 
 const Nav = () => { 
 return ( 
 <div className="App mt-5 mb-0">
       <div className="btn-group" role="group" aria-label="Basic example">
-        <a href="/user" className="btn btn-light">
-          Profile
-        </a>
-        <a href="/fileupload" className="btn btn-light">
-          File Upload
-        </a>
-        <a href="/viewdata" className="btn btn-light">
-          View File
-        </a>
+        <Link to="/user" className="btn btn-light">My Profile</Link>
+        <Link to="/fileupload" className="btn btn-light">Upload File</Link>
+        <Link to="/viewdata" className="btn btn-light">View File</Link>
       </div>
 </div> 
 )
